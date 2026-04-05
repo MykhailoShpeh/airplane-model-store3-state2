4a728b991e0f441e74d822d0ffc6314dcbf65eaf
Mykhailo Shpeh <mykhailoshpeh@gmail.com>
2026-04-05 10:54:57 +0300

    basic styled for colorboxes

---
+++ src/components/ColorBox/ColorBox.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+import css from "./ColorBox.module.css";
+
+export class ColorBox extends Component {
+    render() {
+
+        const { colorBoxes } = this.props;
+        console.log("colorBoxes: ", colorBoxes);
+
+        return (
+            <>
+                {/* //! Блок вибору кольорів */}
+                <div className={css.colorBoxContainer}>
+                    <h2 className={css.colorBoxTitle}>Вибір кольорів</h2>
+                    <p className={css.colorBoxDescription}>Останній обраний колір:<span className={css.colorBoxSelectedColor}>{"Red"}</span></p>
+                    <div className={css.colorBox}>
+                        {colorBoxes.map(item =>
+                            <button className={css.colorBoxButton}>Off</button>
+                    )}
+                    </div>
+                </div>
+                {/* //! Блок обраних кольорів */}
+                <div className={css.selectedColorsContainer}>
+                </div>
+            </>
+        )
+    };
+
+}
--- src/main.jsx
+++ src/main.jsx
@@ -4,14 +4,15 @@ import { createRoot } from 'react-dom/client'
 import '@/index.css'
 import { App } from '@/components/App/App.jsx';
 import { AppCounter } from '@/components/App/AppCounter.jsx';
+import { AppColorBox } from "@/components/App/AppColorBox.jsx"
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter basename="/airplane-model-store3-state2">
-      <App />
+      {/* <App /> */}
       {/* <AppCounter /> */}
-
+      <AppColorBox />
     </BrowserRouter>
   </StrictMode >
 );
