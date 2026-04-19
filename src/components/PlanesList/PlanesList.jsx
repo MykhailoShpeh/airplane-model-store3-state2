de96d9a039e948ebf31fea9297cd609acc07e30b
Mykhailo Shpeh <mykhailoshpeh@gmail.com>
2026-04-19 11:29:50 +0300

    planes cart logic start

--- src/components/PlanesList/PlanesList.jsx
+++ src/components/PlanesList/PlanesList.jsx
@@ -23,8 +23,8 @@ export function PlanesList({ items }) {
                     <Planes
                         urlMain={item.url.main}
                         urlPromotional={item.url.promotional}
-                        urlActual={item.url.actual}
-                        urlActualFull={item.url.actualFull}
+                        urlActual={item.model.actualImages}
+                        urlActualFull={item.model.actualFullImages}
                         nameBrief={item.name.brief}
                         nameFull={item.name.full}
                         nickname={item.name.nickname}
